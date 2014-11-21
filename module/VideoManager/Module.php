05ae0b325b81f8ce7c1b2f2e8e3f4ef0ab16292d
Matthew Setter <matthew.setter@gmail.com>
2014-11-21 20:37:50 +0100

    Override The Layout In A Controller Action
    This commit shows how to override a layout in an individual controller
    action.

--- module/VideoManager/Module.php
+++ module/VideoManager/Module.php
@@ -10,16 +10,6 @@ class Module
         return include __DIR__ . '/config/module.config.php';
     }
 
-    public function init(ModuleManager $manager)
-    {
-        $events = $manager->getEventManager();
-        $sharedEvents = $events->getSharedManager();
-        $sharedEvents->attach(__NAMESPACE__, 'dispatch', function($e) {
-            $controller = $e->getTarget();
-            $controller->layout('video-layout');
-        }, 100);
-    }
-
     public function getAutoloaderConfig()
     {
         return array(
