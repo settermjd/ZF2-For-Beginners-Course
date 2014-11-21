bc59377e182da813352ae2a34fdf30aa257cd59d
Matthew Setter <settermjd@sambia>
2014-11-21 17:47:23 +0100

    Add The Basic Module Configuration
    With this configuration, the module is able to be used. For example,
    each of the actions can be routed to in the browser, and the view
    templates are able to be retrieved to render the output.

---
+++ module/VideoManager/config/module.config.php
@@ -0,0 +1,53 @@
+<?php
+return array(
+    'router' => array(
+        'routes' => array(
+            // The following is a route to simplify getting started creating
+            // new controllers and actions without needing to create a new
+            // module. Simply drop new controllers in, and you can access them
+            // using the path /application/:controller/:action
+            'video' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    'route'    => '/videos',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'VideoManager\Controller',
+                        'controller'    => 'Index',
+                        'action'        => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '/[:controller[/:action][/pages/:page]]',
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'page'       => '[0-9]*',
+                                'perPage'       => '[0-9]*',
+                            ),
+                            'defaults' => array(
+                                'page' => 1
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'VideoManager\Controller\Index' => 'VideoManager\Controller\IndexController'
+        ),
+    ),
+    'view_manager' => array(
+        'template_map' => array(
+
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+);
---
+++ module/VideoManager/src/VideoManager/Controller/IndexController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace VideoManager\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController
+{
+
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+
+    public function deleteAction()
+    {
+        return new ViewModel();
+    }
+
+    public function viewAction()
+    {
+        return new ViewModel();
+    }
+
+    public function searchAction()
+    {
+        return new ViewModel();
+    }
+
+    public function manageAction()
+    {
+        return new ViewModel();
+    }
+
+
+}
+
