a3517be3a91718fd4e21295a97bbbc1906b80485
Matthew Setter <matthew.setter@gmail.com>
2014-12-22 22:24:06 +0100

    Render A Template Within A Template
    This commit shows how to render a template, within another template. It
    makes it easy to display additional output, or to reuse templates across
    different aspects of the application.

--- module/VideoManager/config/module.config.php
+++ module/VideoManager/config/module.config.php
@@ -45,6 +45,7 @@ return array(
     'view_manager' => array(
         'template_map' => array(
             'simple-output' => __DIR__ . '/../view/video-manager/index/simple-output.phtml',
+            'copyright' => __DIR__ . '/../view/video-manager/index/copyright-notice.phtml',
         ),
         'template_path_stack' => array(
             __DIR__ . '/../view',
